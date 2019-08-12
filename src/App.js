6e4ea18f67cd0899f925619447d9f6b874f2e61c
Tatty <tattyklass@gmail.com>
2019-08-12 09:33:45 -0700

    inital file commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <NavBar/>
+        <Switch>  
+          <Route exact path='/about' render= { () =>
+            <About/>
+          }/>
+          <Route exact path='/skills' render= { () =>
+            <Skills/>
+          }/>
+        </Switch>  
+      </div>
+    );
+  } 
 }
 
 export default App;
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => {
+    return ( 
+        <div>
+            Hello from Skills
+        </div>
+     );
+}
+ 
+export default Skills;
