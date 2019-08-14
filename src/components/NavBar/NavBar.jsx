1231ecef1a661915a2e6bc8d4979e3208cf58274
Tatty <tattyklass@gmail.com>
2019-08-13 20:37:55 -0700

    paralax

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,24 +5,24 @@ import './NavBar.css'
 const NavBar = () => {
     return ( 
         <div className="NavBar-outer-container">
-        <div className="NavBar-container">
-            <div className="NavBar-top-container">
-                <div className="NavBar-name">TATANYA KLASS</div>
-                <div className="NavBar-title">Web Developer</div>
-                <div className="NavBar-email">tattyklass@gmail.com</div>
+            <div className="NavBar-container">
+                <div className="NavBar-top-container">
+                    <div className="NavBar-name">TATANYA KLASS</div>
+                    <div className="NavBar-title">Web Developer</div>
+                    <div className="NavBar-email">tattyklass@gmail.com</div>
 
-                <div className="NavBar-contact-container">
-                    <a className="NavBar-contact" href="https://github.com/tatty-k" target="_blank" >GitHub</a>
-                    <a className="NavBar-contact" href="https://linkedin.com/in/tatanyaklass" target="_blank" >LinkedIn</a>
+                    <div className="NavBar-contact-container">
+                        <a className="NavBar-contact" href="https://github.com/tatty-k" target="_blank" >GitHub</a>
+                        <a className="NavBar-contact" href="https://linkedin.com/in/tatanyaklass" target="_blank" >LinkedIn</a>
+                    </div>
+                </div>
+
+                <div className="NavBar-link-container">
+                    <Link className="NavBar-link" to={'/'}>About</Link>
+                    <Link className="NavBar-link" to={'/projects'}>Projects</Link>
+                    <Link className="NavBar-link" to={'/skills'}>Skills</Link>
                 </div>
             </div>
-
-            <div className="NavBar-link-container">
-                <Link className="NavBar-link" to={'/'}>About</Link>
-                <Link className="NavBar-link" to={'/projects'}>Projects</Link>
-                <Link className="NavBar-link" to={'/skills'}>Skills</Link>
-            </div>
-        </div>
         </div>
      );
 }
